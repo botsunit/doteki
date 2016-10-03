4e6d054476ebe10be4bea263c011271750f6dbcf
glejeune <gregoire.lejeune@free.fr>
2016-10-03 16:20:37 +0200

    Add tests

--- test/doteki_overload_tests.erl
+++ test/doteki_overload_tests.erl
@@ -8,7 +8,8 @@
                       {key2, {system, "CUSTOM_ENV_2"}},
                       {key3, {system, "CUSTOM_ENV_3", default}},
                       {key4, {system, "CUSTOM_ENV_4", as, binary}},
-                      {key5, {system, "CUSTOM_ENV_5", as, binary, default}}
+                      {key5, {system, "CUSTOM_ENV_5", as, binary, default}},
+                      {key6, {system, "APP_KEY6", as, binary, default}}
                      ]}
               ]).
 
@@ -18,12 +19,12 @@ doteki_overload_test_() ->
    fun() ->
        [os:unsetenv(X) ||
         X <- ["CUSTOM_ENV_1", "CUSTOM_ENV_2", "CUSTOM_ENV_3", "CUSTOM_ENV_4", "CUSTOM_ENV_5",
-              "APP_KEY1", "APP_KEY2", "APP_KEY3", "APP_KEY4", "APP_KEY5"]]
+              "APP_KEY1", "APP_KEY2", "APP_KEY3", "APP_KEY4", "APP_KEY5", "APP_KEY6"]]
    end,
    fun(_) ->
        [os:unsetenv(X) ||
         X <- ["CUSTOM_ENV_1", "CUSTOM_ENV_2", "CUSTOM_ENV_3", "CUSTOM_ENV_4", "CUSTOM_ENV_5",
-              "APP_KEY1", "APP_KEY2", "APP_KEY3", "APP_KEY4", "APP_KEY5"]]
+              "APP_KEY1", "APP_KEY2", "APP_KEY3", "APP_KEY4", "APP_KEY5", "APP_KEY6"]]
    end,
    [
     fun() ->
@@ -34,7 +35,8 @@ doteki_overload_test_() ->
           ?assertEqual(undefined, doteki:get_env([app, key2])),
           ?assertEqual(default, doteki:get_env([app, key3])),
           ?assertEqual(undefined, doteki:get_env([app, key4])),
-          ?assertEqual(<<"default">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"default">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("CUSTOM_ENV_1", "hello"),
@@ -42,7 +44,8 @@ doteki_overload_test_() ->
           ?assertEqual(undefined, doteki:get_env([app, key2])),
           ?assertEqual(default, doteki:get_env([app, key3])),
           ?assertEqual(undefined, doteki:get_env([app, key4])),
-          ?assertEqual(<<"default">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"default">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("CUSTOM_ENV_2", "hello"),
@@ -50,7 +53,8 @@ doteki_overload_test_() ->
           ?assertEqual(hello, doteki:get_env([app, key2])),
           ?assertEqual(default, doteki:get_env([app, key3])),
           ?assertEqual(undefined, doteki:get_env([app, key4])),
-          ?assertEqual(<<"default">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"default">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("CUSTOM_ENV_3", "hello"),
@@ -58,7 +62,8 @@ doteki_overload_test_() ->
           ?assertEqual(hello, doteki:get_env([app, key2])),
           ?assertEqual(hello, doteki:get_env([app, key3])),
           ?assertEqual(undefined, doteki:get_env([app, key4])),
-          ?assertEqual(<<"default">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"default">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("CUSTOM_ENV_4", "hello"),
@@ -66,7 +71,8 @@ doteki_overload_test_() ->
           ?assertEqual(hello, doteki:get_env([app, key2])),
           ?assertEqual(hello, doteki:get_env([app, key3])),
           ?assertEqual(<<"hello">>, doteki:get_env([app, key4])),
-          ?assertEqual(<<"default">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"default">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("CUSTOM_ENV_5", "hello"),
@@ -74,7 +80,8 @@ doteki_overload_test_() ->
           ?assertEqual(hello, doteki:get_env([app, key2])),
           ?assertEqual(hello, doteki:get_env([app, key3])),
           ?assertEqual(<<"hello">>, doteki:get_env([app, key4])),
-          ?assertEqual(<<"hello">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"hello">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("APP_KEY1", "world"),
@@ -82,7 +89,8 @@ doteki_overload_test_() ->
           ?assertEqual(hello, doteki:get_env([app, key2])),
           ?assertEqual(hello, doteki:get_env([app, key3])),
           ?assertEqual(<<"hello">>, doteki:get_env([app, key4])),
-          ?assertEqual(<<"hello">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"hello">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("APP_KEY2", "world"),
@@ -90,7 +98,8 @@ doteki_overload_test_() ->
           ?assertEqual(world, doteki:get_env([app, key2])),
           ?assertEqual(hello, doteki:get_env([app, key3])),
           ?assertEqual(<<"hello">>, doteki:get_env([app, key4])),
-          ?assertEqual(<<"hello">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"hello">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("APP_KEY3", "world"),
@@ -98,7 +107,8 @@ doteki_overload_test_() ->
           ?assertEqual(world, doteki:get_env([app, key2])),
           ?assertEqual(world, doteki:get_env([app, key3])),
           ?assertEqual(<<"hello">>, doteki:get_env([app, key4])),
-          ?assertEqual(<<"hello">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"hello">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("APP_KEY4", "world"),
@@ -106,7 +116,8 @@ doteki_overload_test_() ->
           ?assertEqual(world, doteki:get_env([app, key2])),
           ?assertEqual(world, doteki:get_env([app, key3])),
           ?assertEqual(world, doteki:get_env([app, key4])),
-          ?assertEqual(<<"hello">>, doteki:get_env([app, key5]))
+          ?assertEqual(<<"hello">>, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
       end
     , fun() ->
           os:putenv("APP_KEY5", "world"),
@@ -114,7 +125,12 @@ doteki_overload_test_() ->
           ?assertEqual(world, doteki:get_env([app, key2])),
           ?assertEqual(world, doteki:get_env([app, key3])),
           ?assertEqual(world, doteki:get_env([app, key4])),
-          ?assertEqual(world, doteki:get_env([app, key5]))
+          ?assertEqual(world, doteki:get_env([app, key5])),
+          ?assertEqual(<<"default">>, doteki:get_env([app, key6]))
+      end
+    , fun() ->
+          os:putenv("APP_KEY6", "world"),
+          ?assertEqual(world, doteki:get_env([app, key6]))
       end
    ]}.
 
