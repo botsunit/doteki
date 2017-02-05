9b395f4135867f08fe51f3e813d64add472e7051
glejeune <gregoire.lejeune@free.fr>
2017-02-05 20:21:30 +0100

    Bump to 1.0.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Doteki.Mixfile do
   def project do
     [
       app: :doteki,
-      version: "1.0.4",
+      version: "1.0.5",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
